526469829eef2b575f4da0b9a3dcf24432eb7c79
wael <waelkourich12@gmail.com>
2019-05-15 19:15:04 +0100

    Add products tab and some transition

---
+++ js/script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded' , ()=>{
+    let controller = new ScrollMagic.Controller();
+    let timeline = new TimelineMax();
+    timeline
+    .to('.blur' , 6 , {
+        top:'0%' 
+    })
+    let scene = new ScrollMagic.Scene({
+        triggerElement:'header',
+        duration:'150%',
+        triggerHook:0
+    })
+    .setTween(timeline)
+    .addTo(controller)
+    let timeline2 = new TimelineMax();
+    timeline2
+    .to(' .features .blur-2' , 6 , {
+        top:'0%' 
+    })
+
+    let scene_2 = new ScrollMagic.Scene({
+        triggerElement:'.features',
+        duration:'150%',
+        triggerHook:0
+    })
+    .setTween(timeline2)
+    .setPin('blur')
+    .addTo(controller)
+})
